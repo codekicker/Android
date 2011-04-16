ef3fac82ab74fffda4281f98074c643b8ff89739
Christian Rackerseder <christian@echooff.de>
2011-04-16 12:08:31 +0200

    PreferenceManager added

--- Codekicker/src/de/codekicker/app/android/config/NotConfiguredException.java
+++
@@ -1,7 +0,0 @@
-package de.codekicker.app.android.config;
-
-public class NotConfiguredException extends Exception {
-	public NotConfiguredException(String message) {
-		super(message);
-	}
-}
---
+++ Codekicker/src/de/codekicker/app/android/preference/PreferenceManager.java
@@ -0,0 +1,49 @@
+package de.codekicker.app.android.preference;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+
+public class PreferenceManager {
+	private static PreferenceManager instance = new PreferenceManager();
+	private static SharedPreferences sharedPreferences;
+	
+	private PreferenceManager() {}
+	
+	public static PreferenceManager getInstance(Context context) {
+		if (sharedPreferences == null) {
+			sharedPreferences = android.preference.PreferenceManager.getDefaultSharedPreferences(context);
+		}
+		return instance;
+	}
+	
+	public String getApiBaseUrl() {
+		return "http://codekicker.de/api/v1/";
+	}
+	
+	public String getAppIdKey() {
+		return "CK-App-ID";
+	}
+	
+	public String getAppId() {
+		return "codekicker.official-app.v1.0";
+	}
+	
+	public String getUsername() {
+		return sharedPreferences.getString("username", "");
+	}
+	
+	public String getPassword() {
+		return sharedPreferences.getString("password", "");
+	}
+	
+	public boolean getIsUserAuthenticated() {
+		return sharedPreferences.getBoolean("UserAuthenticated", false);
+	}
+	
+	public void setIsUserAuthenticated(boolean value) {
+		Editor editor = sharedPreferences.edit();
+		editor.putBoolean("UserAuthenticated", value);
+		editor.commit();
+	}
+}
